60c26b7578472ef5d90b8089ce2a0ffc4180d476
Diogo <diogosf2006@gmail.com>
2026-04-15 11:09:22 -0300

    feat: criado classes de security utilizando JWT

---
+++ src/main/java/com/desafio/obras/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.desafio.obras.security;
+
+import com.desafio.obras.entity.Autor;
+import com.desafio.obras.repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+    // Repositório para acessar dados de usuário no banco de dados
+    @Autowired
+    private AutorRepository autorRepository;
+
+    // Implementação do método para carregar detalhes do usuário pelo e-mail
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Busca o usuário no banco de dados pelo e-mail
+        Autor autor = autorRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Autor não encontrado: " + email));
+
+        // Cria e retorna um objeto UserDetails com base no usuário encontrado
+        return org.springframework.security.core.userdetails.User
+                .withUsername(autor.getEmail()) // Define o nome de usuário como o e-mail
+                .password(autor.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+}
