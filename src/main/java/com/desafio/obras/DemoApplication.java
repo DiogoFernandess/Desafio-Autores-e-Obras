7796cad46d769a7a8834154303f0f74fe23eef6a
Diogo <diogosf2006@gmail.com>
2026-04-16 22:14:24 -0300

    configuração clean da aplicação

--- src/main/java/com/desafio/obras/DemoApplication.java
+++ src/main/java/com/desafio/obras/DemoApplication.java
@@ -3,13 +3,12 @@ package com.desafio.obras;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
