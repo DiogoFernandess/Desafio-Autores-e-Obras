345c7ac51692e2e38b2ee954edca9bce049c6b56
Diogo <diogosf2006@gmail.com>
2026-04-17 23:21:38 -0300

    feat: endpoint de busca de dados de autor por email

--- src/main/java/com/desafio/obras/controller/AutorController.java
+++ src/main/java/com/desafio/obras/controller/AutorController.java
@@ -5,6 +5,7 @@ import com.desafio.obras.security.JwtUtil;
 import com.desafio.obras.security.SecurityConfig;
 import com.desafio.obras.service.AutorService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -44,5 +42,9 @@ public class AutorController {
         return "Bearer" + jwtUtil.generateToken(authentication.getName());
     }
 
-
+    @GetMapping
+    @Operation(summary = "Buscar dados de autor por email", description = "Buscar dados do autor")
+    public ResponseEntity<AutorDTO> buscaUsuarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(autorService.buscaAutorPorEmail(email));
+    }
 }
--- src/main/java/com/desafio/obras/service/AutorService.java
+++ src/main/java/com/desafio/obras/service/AutorService.java
@@ -4,6 +4,7 @@ import com.desafio.obras.converter.AutorConverter;
 import com.desafio.obras.dto.AutorDTO;
 import com.desafio.obras.entity.Autor;
 import com.desafio.obras.exceptions.ConflictException;
+import com.desafio.obras.exceptions.ResourceNotFoundException;
 import com.desafio.obras.repository.AutorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,6 +37,22 @@ public class AutorService {
     }
 
     public boolean verificaEmailExistente(String email){
+
         return autorRepository.existsByEmail(email);
     }
+
+    public AutorDTO buscaAutorPorEmail(String email){
+        try {
+            return autorConverter.paraAutorDTO(
+                    autorRepository.findByEmail(email)
+                            .orElseThrow(
+                                    () -> new ResourceNotFoundException("Email não encontrado" + email)
+                            )
+            );
+        } catch (ResourceNotFoundException e ){
+            throw new ResourceNotFoundException("Email não encontrado" +email);
+        }
+    }
+
+
 }
